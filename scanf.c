b36629363a203349d4c06393ff8b6217ffd4fb0e
MuhoroX <mahmudysdx@gmail.com>
2024-04-08 21:14:13 +0300

    Create scanf.c

---
+++ scanf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int x;
+    printf("x: ");
+    scanf("%i", &x);
+    printf("x: %i\n", x);
+
+
+    char s[5];
+    printf("s: ");
+    scanf("%s,", s);
+    printf("s: %s \n", s);
+
+}
